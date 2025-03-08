ecbb6f372b72317845174c81925594405ed64aee
Eddie Wang <ieddie217@gmail.com>
2025-03-08 13:17:28 +0800

    updated unified body to projectcards

--- src/components/home/ProjectCard.jsx
+++ src/components/home/ProjectCard.jsx
@@ -15,21 +15,13 @@ const ProjectCard = ({ value }) => {
   } = value;
   return (
     <Col md={6}>
-      <Card className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <Card.Body>
+      <Card className="card shadow-lg p-3 mb-5 bg-white rounded" style={{ height: '280px' }}>
+        <Card.Body className="d-flex flex-column">
           <Card.Title as="h5">{name || <Skeleton />} </Card.Title>
-          <Card.Text>{(!description) ? "" : description || <Skeleton count={3} />} </Card.Text>
-          {svn_url ? <CardButtons svn_url={svn_url} /> : <Skeleton count={2} />}
-          {/* {languages_url ? (
-            <Language languages_url={languages_url} repo_url={svn_url} />
-          ) : (
-            <Skeleton count={3} />
-          )}
-          {value ? (
-            <CardFooter star_count={stargazers_count} repo_url={svn_url} pushed_at={pushed_at} />
-          ) : (
-            <Skeleton />
-          )} */}
+          <Card.Text className="flex-grow-1">{(!description) ? "" : description || <Skeleton count={3} />} </Card.Text>
+          <div className="mt-auto">
+            {svn_url ? <CardButtons svn_url={svn_url} /> : <Skeleton count={2} />}
+          </div>
         </Card.Body>
       </Card>
     </Col>
@@ -39,12 +31,6 @@ const ProjectCard = ({ value }) => {
 const CardButtons = ({ svn_url }) => {
   return (
     <div className="d-grid gap-2 d-md-block">
-      {/* <a
-        href={`${svn_url}/archive/master.zip`}
-        className="btn btn-outline-secondary mx-2"
-      >
-        <i className="fab fa-github" /> Clone Project
-      </a> */}
       <a href={svn_url} target=" _blank" className="btn btn-outline-secondary mx-2">
         <i className="fab fa-github" /> Repo
       </a>
@@ -98,46 +84,5 @@ const Language = ({ languages_url, repo_url }) => {
     </div>
   );
 };
-
-// const CardFooter = ({ star_count, repo_url, pushed_at }) => {
-//   const [updated_at, setUpdated_at] = useState("0 mints");
-
-//   const handleUpdatetime = useCallback(() => {
-//     const date = new Date(pushed_at);
-//     const nowdate = new Date();
-//     const diff = nowdate.getTime() - date.getTime();
-//     const hours = Math.trunc(diff / 1000 / 60 / 60);
-
-//     if (hours < 24) {
-//       if (hours < 1) return setUpdated_at("just now");
-//       let measurement = hours === 1 ? "hour" : "hours";
-//       return setUpdated_at(`${hours.toString()} ${measurement} ago`);
-//     } else {
-//       const options = { day: "numeric", month: "long", year: "numeric" };
-//       const time = new Intl.DateTimeFormat("en-US", options).format(date);
-//       return setUpdated_at(`on ${time}`);
-//     }
-//   }, [pushed_at]);
-
-//   useEffect(() => {
-//     handleUpdatetime();
-//   }, [handleUpdatetime]);
-
-//   return (
-//     <p className="card-text">
-//       <a
-//         href={repo_url + "/stargazers"}
-//         target=" _blank"
-//         className="text-dark text-decoration-none"
-//       >
-//         <span className="text-dark card-link mr-4">
-//           <i className="fab fa-github" /> {" "}
-//           <span className="badge badge-dark">{star_count}</span>
-//         </span>
-//       </a>
-//       <small className="text-muted">Updated {updated_at}</small>
-//     </p>
-//   );
-// };
 
 export default ProjectCard;
